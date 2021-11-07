d1bef2ea2de9148eb5b481cfce339ecb5ceadd9e
Tushar Chokshi <tushar239@yahoo.com>
2021-11-07 12:21:59 +0530

    initial

--- src/main/java/com/stocks/service/StocksBasicInformationAggregator.java
+++ src/main/java/com/stocks/service/StocksBasicInformationAggregator.java
@@ -1,11 +1,30 @@
 package com.stocks.service;
 
+import com.stocks.service.dto.StockBasicInformation;
 import com.stocks.service.dto.StocksBasicInformation;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StocksBasicInformationAggregator implements IStocksBasicInformationAggregator<StocksBasicInformation> {
     @Override
     public StocksBasicInformation aggregate(StocksBasicInformation stocks) {
-        // TODO: add aggregation logic
-        return stocks;
+
+        /*final Map<String, StockBasicInformation> companyNameStockInformation = new HashMap<>();
+
+        stocks.getStream().forEach(stock -> {
+            if(companyNameStockInformation.containsKey(stock.getName())) {
+                // TODO: add aggregation logic
+            } else {
+                companyNameStockInformation.put(stock.getName(), stock);
+            }
+        });
+
+        final StocksBasicInformation aggregatedStocksBasicInformation = new StocksBasicInformation();
+        companyNameStockInformation.forEach((key, value) -> aggregatedStocksBasicInformation.add(value));*/
+
+        final StocksBasicInformation aggregatedStocksBasicInformation = new StocksBasicInformation();
+        stocks.getStream().forEach(stock -> aggregatedStocksBasicInformation.add(stock));
+        return aggregatedStocksBasicInformation;
     }
 }
